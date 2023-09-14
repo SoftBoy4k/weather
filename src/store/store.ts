caf426d1a64ebaae08c18f900816334bd23fbb1a
Vladislav Pimoshenko <vladislavpimoshenko@gmail.com>
2023-09-14 20:34:20 +0300

    delete weatherInfo in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weatherReducer from "./weatherCard.slice"
-import newWeatherReducer from "./weatherInfo.slice";
 
 export const store = configureStore({
     reducer: {
-        weatherCard: weatherReducer,
-        newWeatherInfo: newWeatherReducer
+        weatherCard: weatherReducer
     },
 })
